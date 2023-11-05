33dac0ae6caf51fd0533d449c03fc070ae78b4df
Klaudia <kkborowy@gmail.com>
2023-11-05 03:09:11 +0100

    zad 2

--- first year/wstep-do-programowania/lab1/2.py
+++ first year/wstep-do-programowania/lab1/2.py
@@ -1,10 +1,26 @@
+import random
+
 """
 zadanie 2
 Napisz skrypt, który pobiera od użytkownika drogę pokonaną przez samochód oraz średnie
 spalanie (w litrach na 100 km) i wyświetli informację o przewidywanym zużyciu paliwa oraz o szacowanych
 kosztach podróży (cena paliwa 6.5 zł/l).
 """
-droga = float(input("droga = "))
+# droga = float(input("droga = "))
+# spalanie = float(input("srednie spalanie = "))
+
+# zuzycie = (droga * spalanie) / 100
+
+# cena_paliwa = 6.5
+# koszt_podrozy = zuzycie * cena_paliwa
+
+# print(koszt_podrozy)
+
+"""
+Zmodyfikuj skrypt tak, aby długość przejechanej drogi była generowana losowo (liczba
+całkowita z zakresu <1, 1000>
+"""
+droga = random.randint(1, 1000)
 spalanie = float(input("srednie spalanie = "))
 
 zuzycie = (droga * spalanie) / 100
