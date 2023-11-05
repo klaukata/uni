74499c361905c42ed3881cdc67a551a043e6cacc
Klaudia <kkborowy@gmail.com>
2023-11-05 05:10:28 +0100

    3.7

---
+++ first year/wstep-do-programowania/lab3/7.py
@@ -0,0 +1,16 @@
+"""
+Za pomocą pętli for wypisz na ekranie ciągi liczb:
+• 1, 2, 3, ... , 99, 100
+• 100, 99, ... , 2, 1, 0
+• 7, 14, 21, ... , 70, 77
+• 20, 18, ... , 2, 0
+"""
+
+# for x in reversed(range(101)):
+#     print(x, end=" ")
+    
+# for x in range(7, 78, 7):
+#     print(x, end=" ")
+
+for x in reversed(range(0, 21, 2)):
+    print(x, end=" ")
