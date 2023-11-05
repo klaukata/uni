02fc62f78f8c6d3edb3a5884e1265b79750ca4d2
Klaudia <kkborowy@gmail.com>
2023-11-05 06:54:10 +0100

    3.9

---
+++ first year/wstep-do-programowania/lab3/9.py
@@ -0,0 +1,25 @@
+"""
+Napisz program, który wyświetli „choinkę” jak poniżej. Wysokość „choinki” powinna
+być podawana z klawiatury
+Wersja łatwiejsza (trójkąt):
+4
+*
+* *
+* * *
+* * * *
+Wersja trudniejsza:
+4
+   *
+  * *
+ * * *
+* * * *
+"""
+
+# wersja łatwiejsza
+
+# liczba = int(input("liczba? = "))
+
+# for x in range(liczba):
+#     for y in range(x+1):
+#         print("*", end='')
+#     print('')
